bb8a7d2b1c12d348737a79e58b13f2fcd9efa79e
AliengirlLiv <oliviagracewatkins@gmail.com>
2016-12-30 15:07:14 -0800

    started on home page

--- www/js/index.js
+++ www/js/index.js
@@ -1,2 +1,7 @@
 // SOMETHING USEFUL ABOUT ADDING PLUGINS AND STUFF
 // http://stackoverflow.com/questions/29970885/cordova-5-0-0-which-files-should-i-commit-to-git
+
+function pleaseWork() {
+	console.log("YAY!");
+	$.mobile.pageContainer.pagecontainer("change", "#home", {});
+}
