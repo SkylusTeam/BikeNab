1780dbd74d51bce75d6ff54374f42a70f55d4915
AliengirlLiv <oliviagracewatkins@gmail.com>
2016-12-31 01:46:33 -0800

    Added registration and profile pages, not great yet

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,8 @@
 // SOMETHING USEFUL ABOUT ADDING PLUGINS AND STUFF
 // http://stackoverflow.com/questions/29970885/cordova-5-0-0-which-files-should-i-commit-to-git
 
-function goHome() {
+function goSomewhere(screen) {
 	console.log("YAY!");
-	$.mobile.pageContainer.pagecontainer("change", "#home", {});
+	$.mobile.pageContainer.pagecontainer("change", screen, {});
 }
 
