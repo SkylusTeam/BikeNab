2616ce09ac4838432f020cc63cd1f39ac236b238
AliengirlLiv <oliviagracewatkins@gmail.com>
2016-12-30 16:06:45 -0800

    Fixed unclickable button problem

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,8 @@
 // SOMETHING USEFUL ABOUT ADDING PLUGINS AND STUFF
 // http://stackoverflow.com/questions/29970885/cordova-5-0-0-which-files-should-i-commit-to-git
 
-function pleaseWork() {
+function goHome() {
 	console.log("YAY!");
 	$.mobile.pageContainer.pagecontainer("change", "#home", {});
 }
+
