e09f4f48e09023346b31fddcf7e6ce13d2962060
AliengirlLiv <oliviagracewatkins@gmail.com>
2017-03-09 20:06:40 -0800

    Split js file

---
+++ www/js/handlebarsHelpers.js
@@ -0,0 +1,85 @@
+
+  ///////////////////////////////////////////
+ ////		HANDLEBARS HELPERS			////
+///////////////////////////////////////////
+
+
+
+
+//Only show element if 2 params are ===
+Handlebars.registerHelper('IF', function(var1, operator, var2, options) {
+	switch(operator) {
+		case "===":
+			return (var1 === var2) ? options.fn(this) : null;
+			//If we need more operations, add them
+		case "!=":
+			return (var1 != var2) ? options.fn(this) : null;
+		case ">=":
+			return (var1 >= var2) ? options.fn(this) : null;
+		default:
+			return null;
+	}
+})
+
+
+Handlebars.registerHelper('no', function(condition, text) {
+	if (condition === undefined || condition === "") {
+		return text;
+	} else {
+		return;
+	}
+});
+
+
+Handlebars.registerHelper('exists', function(condition, text) {
+	if (condition !== undefined || condition === "") {
+		return;
+	} else {
+		return text;
+	}
+});
+
+
+// Handlebars.registerHelper('exists', function(condition, text) {
+// 	if (condition !== undefined) {
+// 		return;
+// 	} else {
+// 		return text;
+// 	}
+// });
+
+
+//Converts a word to all lowercase
+Handlebars.registerHelper('lowercase', function(word) {
+	return word.toLowerCase();
+});
+
+
+//Makes the first letter of the word capital
+Handlebars.registerHelper('capitalize', function(word) {
+	if (!word) {
+		return "";
+	}
+	return word.charAt(0).toUpperCase() + word.slice(1);
+});
+
+
+
+
+// Reads params for whether the owner and/or police should be contacted if the bike is recovered
+// Formats them into a string for the report log
+Handlebars.registerHelper('contact', function(contactOwner, contactPolice) {
+	if (contactOwner) {
+		if (contactPolice) {
+			return "Contact owner and contact local police";
+		} else {
+			return "Contact owner";
+		}
+	} else {
+		if (contactPolice) {
+			return "Contact local police";
+		} else {
+			return "no specified instructions";
+		}
+	}
+});
---
+++ www/js/reachedPage.js
@@ -0,0 +1,311 @@
+
+
+  ///////////////////////////////////////////
+ ////		reachedPage FUNCTIONS		////
+///////////////////////////////////////////
+
+
+// These functions specify what should happen once we reach a new
+// page, typically involving shoving data into a Handlebars template
+
+
+
+//Huge mega switch statement telling what we should do when each page loads
+$("body").on('pagecontainerbeforeshow', function(event, data) {
+	switch (data.toPage[0].id) {
+		case 'bike-detail':
+			return reachedBikeDetail();
+		case 'edit-bike-info':
+			return reachedEditBikeInfo();
+		case 'register':
+			return reachedRegisterBike();
+		case 'past-reports':
+			return reachedPastReports();
+		case 'reports':
+			return loadUser(reachedReports);
+		case 'report':
+			return reachedReport();
+		case 'edit-report':
+			return reachedEditReport();
+		case 'unregister':
+			return reachedUnregister();
+		case 'profile':
+			return reachedProfile();
+		case 'my-bikes':
+			return reachedMyBikes();
+		case 'lookup':
+			return reachedLookup();
+		case 'contact':
+			return reachedContact();
+		case 'settings':
+			return reachedSettings();
+		case 'le-create-account':
+			return reachedLeCreateAccount();
+		case 'create-account':
+			return reachedCreateAccount();
+		case 'le-profile':
+			return reachedLeProfile();
+		case 'home':
+			return reachedHome();
+		case 'le-reports':
+			return reachedLeReports();
+		default:
+			return;
+	}
+});
+
+
+
+//TODO: Can we reuse code among any of these?
+
+function insertTemplate(data, templateName, containerID) {
+	let template = Handlebars.templates[templateName];
+	$(containerID).html(template(data));
+	$(containerID).enhanceWithin();
+}
+
+
+function reachedLeReports() {
+	insertTemplate({reports: userData.reports, following: false}, "leReports", "#le-reports-container");
+	insertTemplate({reports: userData.reports, following: true}, "leReports", "#le-area-reports-container");
+}
+
+
+function reachedHome() {
+	insertTemplate({police: userData.police}, "home", "#home-container");
+}
+
+
+
+
+
+
+function reachedBikeDetail() { //*kangaroo
+
+	var serial = sessionStorage.serial;
+	var currBike;
+	var getBike = firebase.database().ref('/bikes/' + serial);
+	getBike.on("value", function(theBike) {
+		currBike = theBike.val();
+		if (currBike) {
+			//Get images
+			var user = firebase.auth().currentUser;
+			var userId = user.uid;
+			var imagePath = firebase.storage().ref('/users/' + userId + '/bikes/' + currBike.serial);
+			
+			imagePath.child('bikePic').getDownloadURL().then(function(url) {
+				console.log("bikePic should be working!");
+				currBike.bikePic = url;
+			}).catch(function(error) {
+				console.log("error:", error.code);
+			}).then(function(url) {
+				console.log("second thing!");
+				imagePath.child('bikeOwnerPic').getDownloadURL().then(function(url) {
+				console.log("owner pic should be working");
+				currBike.bikeOwnerPic = url;
+			}).catch(function(error) {
+				console.log("error:", error.code);
+			}).then(function(url) {
+				console.log("after first error");
+				imagePath.child('bikeSerialPic').getDownloadURL().then(function(url) {
+				currBike.bikeSerialPic = url;
+			}).catch(function(error) {
+				console.log("error:", error.code);
+			}).then(function(url) {
+				imagePath.child('receiptPic').getDownloadURL().then(function(url) {
+				currBike.receiptPic = url;
+			}).catch(function(error) {
+				console.log("error:", error.code);
+			})
+			}).then(function(url) {
+				console.log("stuff after thens");
+				//If it's your bike, have a parameter that says that
+				if(currBike.ownerid == firebase.auth().currentUser.uid) //could be a one liner
+					currBike.mine = true;
+				else
+					currBike.mine = false;
+
+				insertTemplate(currBike, "bikeDetail", "#bike-detail-container");
+			})
+			})
+			});
+
+			
+
+
+		}
+	})
+
+}
+
+
+function reachedEditBikeInfo() {
+	let bikesList = userData.bikes;
+	for (bike of bikesList) {
+		if (bike.serial === sessionStorage.serial) {
+			bike.app = isApp();
+			insertTemplate(bike, "bikeInfo", "#edit-bike-info-container");
+		}
+	}
+}
+
+
+function reachedRegisterBike() {
+	insertTemplate(null, "bikeInfo", "#register-bike-info-container");
+}
+
+
+function reachedPastReports() {
+	var data = loadedReports;
+	//fairly confident this does nothing
+	/*
+	data.reports.forEach(function(report, index, reportArr) {
+		for (bike of data.bikes) {
+			if (bike.serial === report.serial) {
+				reportArr[index].bike = bike;
+			}
+		}
+	})
+	*/
+	insertTemplate(data, "reports", "#past-reports-container");
+}
+
+//TODO: Lots of repeat between here and the function above
+function reachedReports() {
+	var data = loadedUser;
+
+	if (loadedReports) {
+		data.reports = loadedReports.reports;
+
+		data.reports.forEach(function(report, index, reportArr) {
+			for (bike of data.bikes) {
+				if (bike.serial === report.serial) {
+					reportArr[index].bike = bike;
+				}
+			}
+		});
+		insertTemplate(data, "reports", "#reports-container");
+		insertTemplate(userData, "reportABike", "#report-a-bike-container");
+	} else {
+		var user = firebase.auth().currentUser;
+		var userId = user.uid;
+		var reports = firebase.database().ref('/users/' + userId+'/reports');
+		var reportDict = {reports:[]};
+		if (reports) {
+			reports.on("value", function(snapshot) {
+				if(snapshot.val()) {
+					jQuery.each(snapshot.val(), function() {
+						var getReport = firebase.database().ref('/reports/'+this.toString());
+						getReport.on("value", function(theReport) {
+							reportDict["reports"].push( theReport.val());
+						});
+					});
+				}
+				data.reports = reportDict
+				console.log(data);
+				insertTemplate(data, "reports", "#reports-container");
+				insertTemplate(userData, "reportABike", "#report-a-bike-container");
+			}, function (error) {
+				console.log("Error: " + error.code);
+			});
+
+		}
+	}
+
+}
+
+
+function reachedReport() {
+	let data = null;
+	if (sessionStorage.newReportSerial) {
+		data={serial: sessionStorage.newReportSerial};
+		sessionStorage.removeItem("newReportSerial");
+	}
+	insertTemplate(data, "report", "#report-container");
+}
+
+
+function reachedEditReport() {
+	for (report of userData.reports) {
+		if (report.reportID === sessionStorage.reportID) {
+			insertTemplate(report, "report", "#edit-report-container");
+		}
+	}
+}
+
+function reachedUnregister() {
+	insertTemplate({serial: sessionStorage.serial , model:sessionStorage.model}, "unregister", "#unregister-container");
+}
+
+function reachedProfile() {
+	var profileData = loadedUser;
+	insertTemplate(profileData, "profile", "#profile-container");
+}
+
+function reachedMyBikes() {
+	var user = firebase.auth().currentUser;
+
+	if (user) {
+		var userId = user.uid;
+		bikeDict = loadedBikes;
+		insertTemplate(bikeDict, "myBikes", "#my-bikes-container");
+		} else {
+			insertTemplate(userData, "myBikes", "#my-bikes-container");
+		}
+}
+
+function reachedLookup() {
+	const privacy = getPrivacySetting(normalData.bikes[0], normalData.privacy);
+	if (privacy === -1) {
+		insertTemplate(null, "lookup", "#lookup-container");
+	} else {
+		const data = {privacy: privacy, owner: normalData.profile, report: normalData.reports[0], bike: normalData.bikes[0]};//TODO: Take this line out later, replace w. database call
+		insertTemplate(data, "lookup", "#lookup-container");
+	}
+}
+
+function reachedContact() {
+	insertTemplate(userData.profile, "contact", "#contact-container");
+}
+
+function reachedSettings() {
+	insertTemplate(userData, "settings", "#settings-container");
+}
+
+function reachedLeCreateAccount() {
+	insertTemplate(null, "createAccount", "#le-create-account-container");
+	insertTemplate(null, "lePersonal", "#le-personal-create-container");
+}
+
+function reachedLeProfile() {
+	insertTemplate(userData.profile, "lePersonal", "#le-personal-profile-container");
+}
+
+function reachedCreateAccount() {
+	insertTemplate(null, "createAccount", "#create-account-container");
+}
+
+
+//Return the relevant privacy setting (-1, 0, 1, or 2), taking into account
+// 		a) Is the person police?
+//		b) Owner's listed privacy settings
+//		c) Bike's status (stolen or okay)
+function getPrivacySetting(bike, privacy) {
+	const stolen = (bike.status === "Missing" || bike.status === "Stolen");
+	if(stolen) {
+		if (userData.police) {
+			return privacy.policeSearchStolen;
+		} else {
+			return privacy.individualSearchStolen;
+		}
+	} else {
+		if (userData.police) {
+			return privacy.policeSearchNormal;
+		} else {
+			return -1; //Individuals can't see unstolen bikes.
+		}
+	}
+}
+
+
+
